713fd67b283dab0b91b5dd6f30f6d126aa8ef80f
Hector Hernandez <hectorhernandezpenades@gmail.com>
2019-03-06 19:35:03 +0000

    feature to display the option to update a question fixed and make it available  only when
    there are no answers

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2019_03_06_131826) do
-
 ActiveRecord::Schema.define(version: 2019_03_06_151819) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -92,12 +88,9 @@ ActiveRecord::Schema.define(version: 2019_03_06_151819) do
     t.string "last_name"
     t.string "location"
     t.text "bio"
-
     t.float "latitude"
     t.float "longitude"
-
     t.integer "upvote"
-
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
