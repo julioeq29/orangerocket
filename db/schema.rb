6dece67cefc1fecbee28d883e5ab32976ef965a3
gina chen <ginachen588@gmail.com>
2019-03-07 12:31:38 +0000

    fixed the map

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_03_06_154619) do
 
   # These are extensions that must be enabled in order to support this database
@@ -90,10 +89,10 @@ ActiveRecord::Schema.define(version: 2019_03_06_154619) do
     t.string "last_name"
     t.string "location"
     t.text "bio"
-    t.boolean "admin", default: false, null: false
     t.float "latitude"
     t.float "longitude"
     t.integer "upvote"
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
