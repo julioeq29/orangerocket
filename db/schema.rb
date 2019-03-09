19cb1c57cb57f1a19abbcc76db81d6d4fe78a1ee
gina chen <ginachen588@gmail.com>
2019-03-09 10:54:29 +0000

    photo-upload

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_07_124644) do
+ActiveRecord::Schema.define(version: 2019_03_09_104218) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -96,10 +96,11 @@ ActiveRecord::Schema.define(version: 2019_03_07_124644) do
     t.string "last_name"
     t.string "location"
     t.text "bio"
-    t.boolean "admin", default: false, null: false
     t.float "latitude"
     t.float "longitude"
     t.integer "upvote"
+    t.boolean "admin", default: false, null: false
+    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
