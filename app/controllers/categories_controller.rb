b05d8f28d25c6f2b2f7d7f9d184d3114d7af148a
Hector Hernandez <hectorhernandezpenades@gmail.com>
2019-03-11 17:06:39 +0000

    filtering the questions results depending on the tag selected function implemented

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -9,15 +9,16 @@ class CategoriesController < ApplicationController
   end
 
   def show
-    # raise
     @category = Category.find(params[:id])
     @tags = Tag.all
     authorize @category
     @questions = Question.all.where(category: @category).order(created_at: :desc)
     if params[:tag].nil?
       @articles = Article.all.where(category: @category)
+      @questions = Question.all.where(category: @category)
     else
       @articles = Article.joins(:category).where(tag: params[:tag])
+      @questions = Question.joins(:category).where(tag: params[:tag])
     end
   end
 end
