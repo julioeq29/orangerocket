efc34afec0fdd15e3e053d2444e195ecd4ff9d18
Hector Hernandez <hectorhernandezpenades@gmail.com>
2019-03-14 10:23:27 +0000

    dependency of question_tags from question fixed

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,7 +3,7 @@ class Question < ApplicationRecord
   belongs_to :category
   has_many :answers, dependent: :destroy
   has_many :question_tags
-  has_many :tags, through: :question_tags
+  has_many :tags, through: :question_tags, dependent: :destroy
 
   validates :content, presence: true
   include PgSearch
